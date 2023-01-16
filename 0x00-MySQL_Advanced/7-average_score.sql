88605a0375db40a7e6abd2883a042a96b3ffc68b
flovian <45689069+flovian@users.noreply.github.com>
2023-01-16 17:32:07 +0300

    changes

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,13 +1,14 @@
 --Creates a stored procedure ComputeAverageScoreForUser that
 -- computes and stores the average score for a student.
-DELIMITER //
-CREATE PROCEDURE AddBonus (IN user_id int, IN project_name varchar(255), IN score float)
+delimiter //
+CREATE PROCEDURE ComputeAverageScoreForUser (
+	IN user_id INT
+)
 BEGIN
-    INSERT INTO projects (name)
-    SELECT project_name FROM DUAL
-
-    WHERE NOT EXISTS (SELECT * FROM projects WHERE name = project_name);
-    INSERT INTO corrections (user_id, project_id, score)
-    VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
+	UPDATE users
+	SET average_score = (SELECT SUM(score) / COUNT(*)
+   						 FROM corrections AS c
+   						 WHERE c.user_id = user_id)
+	WHERE id = user_id;
 END;
 //
