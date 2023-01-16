2a7a6db82361694fa6b1fda3199ac97df154ce05
flovian <45689069+flovian@users.noreply.github.com>
2023-01-16 16:56:52 +0300

    Complete tasks

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- SQL script that lists all bands with Glam rock as their main style,
+-- ranked by their longevity
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
---
+++ 0x00-MySQL_Advanced/README.md
@@ -0,0 +1,3 @@
+# 0x00. MySQL advanced
+
+Continued tasks for learning advanced MySQL features.
