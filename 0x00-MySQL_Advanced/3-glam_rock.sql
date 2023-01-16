72888c40d306059e3a1b52ae9b2eac6c5b7e8181
flovian <45689069+flovian@users.noreply.github.com>
2023-01-16 17:21:31 +0300

    Correction

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,7 @@
--- SQL script that lists all bands with Glam rock as their main style,
--- ranked by their longevity
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
-    FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-    ORDER BY lifespan DESC;
+-- SQL script that lists all bands with Glam rock as their main style,ranked by their longevity
+
+SELECT band_name,
+ifnull(split, 2022) - ifnull(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
